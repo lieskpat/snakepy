ef34f35c6c514ca5b4e7f5dc3307fc94b092ec26
lies <patrick.lieske@web.de>
2020-05-17 15:17:57 +0200

    Unresolved side effects in thread with Key listener

--- Snake.py
+++ Snake.py
@@ -2,7 +2,7 @@ import numpy
 from abc import ABC
 import os
 import threading
-import pygame
+import click
 
 
 class Observable(ABC):
@@ -14,13 +14,13 @@ class Observable(ABC):
     def detach(self, observer):
         self.observer_list.remove(observer)
 
-    def notify(self):
+    def notify(self, subject):
         for observer in self.observer_list:
-            observer.update()
+            observer.update(subject)
 
 
 class ObserverInterface(ABC):
-    def update(self):
+    def update(self, subject):
         pass
 
 
@@ -32,24 +32,31 @@ class GameRoundTimer(Observable, threading.Thread):
 
     def run(self):
         while not self.stopped.wait(self.time_to_run):
-            self.notifier()
+            super().notify(self)
 
-    def notifier(self):
-        super().notify()
 
-    def stop_timer(self):
-        pass
+class KeyListener(Observable, threading.Thread):
+    def __init__(self, event):
+        threading.Thread.__init__(self)
+        self.key = None
+        self.stopped = event
+
+    def run(self):
+        while True:
+            self.key = click.getchar()
+            if self.key == "h" or self.key == "p" or self.key == "k" or self.key == "l":
+                super().notify(self)
 
 
 class Cell(object):
 
-    def __init__(self, x_position, y_position, cellType):
-        self.x_position = x_position
-        self.y_position = y_position
-        self.cellType = cellType
+    def __init__(self, row_position, col_position):
+        self.row_position = row_position
+        self.col_position = col_position
+        self.cellType = CellType.EMPTY
 
     def get_position(self):
-        return "x= " + format(self.x_position) + " " + "y= " + format(self.y_position)
+        return "row_item= " + format(self.row_position) + " " + "col_item= " + format(self.col_position)
 
     def get_cell_type(self):
         return self.cellType
@@ -63,26 +70,28 @@ class Cell(object):
 
 class SnakeCell(Cell):
 
-    def __init__(self, x_position, y_position):
-        Cell.__init__(self, x_position, y_position, CellType.SNAKE)
+    def __init__(self, row_position, col_position):
+        super().__init__(row_position, col_position)
+        self.set_cell_type(CellType.SNAKE)
 
 
 class SnakeHead(SnakeCell):
 
-    def __init__(self, x_position, y_position):
-        SnakeCell.__init__(self, x_position, y_position)
-        Cell.cellType = CellType.HEAD
-        self.direction = Direction.UP
+    def __init__(self, row_position, col_position, direction):
+        super().__init__(row_position, col_position)
+        self.set_cell_type(CellType.HEAD)
+        self.direction = direction
 
     def new_head_position(self):
         if self.direction == Direction.DOWN:
-            self.y_position = self.y_position + 1
+            self.row_position = self.row_position + 1
         elif self.direction == Direction.UP:
-            self.x_position = self.x_position - 1
+            self.row_position = self.row_position - 1
+            print(self.get_position_with_cell_type())
         elif self.direction == Direction.LEFT:
-            self.x_position = self.x_position - 1
+            self.col_position = self.col_position - 1
         elif self.direction == Direction.RIGHT:
-            self.x_position = self.x_position + 1
+            self.col_position = self.col_position + 1
 
     def move_down(self):
         if self.direction != Direction.UP:
@@ -133,7 +142,7 @@ class GameField(object):
         self.game_field = numpy.zeros(shape=(self.rows, self.cols), dtype=Cell)
         for i in range(self.rows):
             for j in range(self.cols):
-                self.game_field[i][j] = Cell(i, j, CellType.EMPTY)
+                self.game_field[i][j] = Cell(i, j)
 
     def get_cell(self, row, col):
         return self.game_field[row][col]
@@ -167,36 +176,57 @@ class Game(ObserverInterface):
 
     def play_a_round(self):
         os.system('clear')
-        self.move_snake_head()
+        # click.clear()
+        # self.key_listener()
         print(self.game_field.show_game_field())
+        self.move_snake_head()
+        self.is_collision()
 
         # self.is_collision()
         # inform the controller and view to show updated GameField
 
     def move_snake_head(self):
         # old Head position
-        self.game_field.get_cell(self.snake.snakeHead.x_position,
-                                 self.snake.snakeHead.y_position).set_cell_type(CellType.EMPTY)
+
+        self.game_field.get_cell(self.snake.snakeHead.row_position,
+                                 self.snake.snakeHead.col_position).set_cell_type(CellType.EMPTY)
         # new Head position first and then update the GameField
         self.snake.snakeHead.new_head_position()
-        self.game_field.get_cell(self.snake.snakeHead.x_position,
-                                 self.snake.snakeHead.y_position).set_cell_type(CellType.HEAD)
+        self.game_field.get_cell(self.snake.snakeHead.row_position,
+                                 self.snake.snakeHead.col_position).set_cell_type(CellType.HEAD)
 
     def is_collision(self):
-        self.stopFlag.set()
+        if self.snake.snakeHead.row_position < 0 and \
+                self.snake.snakeHead.row_position < self.game_field.rows:
+            print("out of range" + " " + str(self.snake.snakeHead.row_position))
 
-    def update(self):
-        self.play_a_round()
+            self.stopFlag.set()
+
+    def update(self, subject):
+        if isinstance(subject, GameRoundTimer):
+            self.play_a_round()
 
 
-class SnakeController(object):
+class SnakeController(ObserverInterface):
     # maybe start here the timer task and play a round
-    def __init__(self, game, view):
+    def __init__(self, game):
         self.game = game
-        self.view = view
+        # self.view = view
+        self.stopFlag = threading.Event()
+        self.key_listener = KeyListener(self.stopFlag)
+        self.key_listener.attach(self)
+        # unresolved side effects when i used KeyListener thread
+        # self.key_listener.start()
 
-    def key_listener(self):
-        pass
+    def which_next_direction(self, key):
+        if key == "h":
+            self.move_left()
+        elif key == "j":
+            self.move_down()
+        elif key == "k":
+            self.move_up()
+        elif key == "l":
+            self.move_right()
 
     def move_up(self):
         self.game.snake.snakeHead.move_up()
@@ -210,22 +240,14 @@ class SnakeController(object):
     def move_right(self):
         self.game.snake.snakeHead.move_right()
 
+    def update(self, subject):
+        if isinstance(subject, KeyListener):
+            # print(subject.key)
+            self.which_next_direction(subject.key)
+
 
 class View(object):
     pass
 
 
-Game(GameField(25, 35), Snake(SnakeHead(24, 14)))
-
-# view = View()
-# controller = SnakeController(game, view)
-
-# field.get_cell(4, 14).set_cell_type(CellType.HEAD)
-# print(field.show_game_field())
-# for x in range(field.rows):
-#    for y in range(field.cols):
-#        cell = field.get_cell(x, y)
-#        print(cell.get_position_with_cell_type())
-# print(cell.cellType)
-# print(field.get_cell(0, 3))
-# print(field.get_cell(4, 4))
+SnakeController(Game(GameField(20, 20), Snake(SnakeHead(19, 19, Direction.UP))))
